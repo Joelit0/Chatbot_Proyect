b9c51819ee195f68e1c51c9f8bb484bb0e854fa5
luanaacuna <101676381+luanaacuna@users.noreply.github.com>
2022-06-09 20:26:55 -0300

    Test (#12)
    * prueba
    
    * ahora si
    
    Co-authored-by: [Luana Acuña] <[luana.acuna1@gmail.com]>

--- src/Library/Ship.cs
+++ src/Library/Ship.cs
@@ -1,6 +1,40 @@
 namespace ChatBotProject
 {
-  public class Ship
-  {
-  }
+    public class Ship
+    {
+        protected int score;
+        protected int large;
+        private bool isAlive;
+
+        public Ship(int score, int large)
+        {
+            this.score = score;
+            this.large = large;
+        }
+        public int Score    // Obtenemos el valor del barco.
+        {
+            get 
+            {
+                return this.score;
+            }
+        }
+        public int Large    // Obtenemos el largo del barco.
+        {
+            get 
+            {
+                return this.large;
+            }
+        }
+        public bool checkIsAlive(int Large) // Aquí chequeamos el largo del barco, si es igual a cero, quiere decir que ya se hundió completamente. 
+        {
+            if (Large==0)
+            {
+                return isAlive = false;
+            }
+            else
+            {
+                return isAlive = true;
+            }       
+        }
+    }
 }
