c6b82bce129dfb6d9e1d6d3548703079f3024f43
SantiRDL <santiago.rodriguezdl@correo.ucu.edu.uy>
2022-06-09 21:02:58 -0300

    a

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,7 @@
+namespace Library
+{
+    public class ConsolePrinter : IPrinter
+    {
+        void PrintTable(Table table);
+    }
+}
---
+++ src/Library/IPrinter.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Library
+{
+    public interface IPrinter
+    {
+        void PrintTable(Table table)
+        {
+            Console.WriteLine(table);
+        }
+    }
+}
