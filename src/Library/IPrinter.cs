a5ab0922eff11af98e652d8882ad8390164ee3b4
Joelit0 <joel.alayon@moove-it.com>
2022-06-22 01:07:42 -0300

    Update ConsolePrinter and IPrinter class

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -2,6 +2,10 @@ namespace ChatBotProject
 {
   public interface IPrinter
   {
+    /// <summary>
+    /// Se usa el Dependency Inversion Principle, ya que esta interfaz se implementa en la clase ConsolePrinter de forma que la clase ConsolePrinter no dependa de otra clase que no sea una abstracción.
+    /// Esta clase es una interfaz para implementar en la clase ConsolePrinter.
+    /// </summary>
     void printBoard(Board board);
   }
 }
