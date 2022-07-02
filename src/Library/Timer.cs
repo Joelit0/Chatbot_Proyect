a9aff7e5f55e10035fa4c7f79dd0f476a4cbc0b7
Joelit0 <joel.alayon@moove-it.com>
2022-07-02 12:58:18 -0300

    Add timerClock to Timer class

--- src/Library/Timer.cs
+++ src/Library/Timer.cs
@@ -3,10 +3,11 @@ namespace ChatBotProject
   /// <summary>
   /// Esta clase Timer es un temporizador.
   /// </summary>
-  public class Timer 
+  public class Timer
   {
     private int Mins;
     private int Secs;
+    private System.Timers.Timer timerClock;
 
     /// <summary>
     /// Constructor timer.
@@ -16,7 +17,10 @@ namespace ChatBotProject
     public Timer(int mins, int secs)
     {
       this.Mins = mins;
-      this.Secs = secs; 
+      this.Secs = secs;
+      this.timerClock = new System.Timers.Timer();
+      this.timerClock.Elapsed += OnTimedEvent;
+      this.timerClock.AutoReset = false;
     }
 
     /// <summary>
@@ -51,6 +55,34 @@ namespace ChatBotProject
     public void setSecs(int secs)
     {
       this.Secs = secs;
+    }
+
+    /// <summary>
+    /// Se encarga de comenzar el timer. Setea el tiempo basado en los atributos minutos y segundos, luego habilita el timer.
+    /// </summary>
+    public void startTimer()
+    {
+      // Duración del timer
+      this.timerClock.Interval = ((this.Mins * 60) + this.Secs) * 1000;
+
+      // Habilitar el timer
+      this.timerClock.Enabled = true;
+    }
+
+    /// <summary>
+    /// Se encarga de detener el timer. Es el proceso inverso de habilitar el timer. En vez de cargar true en Eneabled, carga false
+    /// </summary>
+    public void stopTimer()
+    {
+      // Deshabilitar el timer
+      this.timerClock.Enabled = false;
+    }
+
+    /// <summary>
+    /// Este método es el encargado de manejar la interrupción del timer. Es decir, cuando el timer termine "caerá" en este método
+    /// </summary>
+    private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+    {
     }
   }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -16,26 +16,9 @@ namespace ChatBotProject
   {
     private static TelegramBotClient botClient;
     private static IHandler firstHandler;
-    private static System.Timers.Timer aTimer;
 
     static void Main(string[] args)
     {
-      // Create a timer and set a one second interval.
-      aTimer = new System.Timers.Timer();
-      aTimer.Interval = 1000;
-
-      // Hook up the Elapsed event for the timer. 
-      aTimer.Elapsed += OnTimedEvent;
-
-      // Have the timer fire repeated events (true is the default)
-      aTimer.AutoReset = true;
-
-      // Start the timer
-      aTimer.Enabled = true;
-
-      Console.WriteLine("Press the Enter key to exit the program at any time... ");
-      Console.ReadLine();
-
       // ===========================================================
       // Lógica del juego
       // List<User> users = new List<User>();
@@ -83,11 +66,6 @@ namespace ChatBotProject
       // // Terminamos el bot.
       // cts.Cancel();
       // // ===========================================================
-    }
-    
-    private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
-    {
-      Console.WriteLine("La partida finalizo");
     }
 
     /// <summary>
