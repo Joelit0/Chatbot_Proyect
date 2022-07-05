be441b876cd36d8498c40dc91959227c3d329e35
Joelit0 <joel.alayon@moove-it.com>
2022-07-05 00:18:48 -0300

    Bot attack & Finish GameVsIA

--- src/Library/GameVsIA.cs
+++ src/Library/GameVsIA.cs
@@ -8,6 +8,7 @@ namespace ChatBotProject
     private string Winner;
     private Board PlayerBoard;
     private Board BotBoard;
+    private List<string> BotAtacks;
     private TelegramPrinter telegramPrinter;
 
     public GameVsIA(User player)
@@ -17,25 +18,45 @@ namespace ChatBotProject
       this.PlayerBoard = new Board(10, 10);
       this.BotBoard = new Board(10, 10);
       this.telegramPrinter = new TelegramPrinter();
+      this.BotAtacks = new List<string>() {};
+
+      this.PlayerBoard.showShips();
+      this.BotBoard.hideShips();
     }
 
     public void printPlayerBoard()
     {
-      this.PlayerBoard.showShips();
-
       this.telegramPrinter.printBoard(this.PlayerBoard, this.Player.ID);
+    }
+
+    public void printBotBoard()
+    {
+      this.telegramPrinter.printBoard(this.BotBoard, this.Player.ID);
+    }
+
+    public void botAttack()
+    {
+      bool invalidBotAttack = true;
+      string attackPosition = "";
+
+      while(invalidBotAttack)
+      {
+        string headerLetters = this.PlayerBoard.getHeaderLetters().Substring(0, 9);
+        int randomLetterIndex = new Random().Next(0, 9);
+        int randomRowIndex= new Random().Next(1, 10);
+        attackPosition = $"{headerLetters[randomLetterIndex]}{randomRowIndex}";
+
+        invalidBotAttack = this.BotAtacks.Contains(attackPosition);
+      }
+
+      this.BotAtacks.Add(attackPosition);
+  
+      this.PlayerBoard.attack(attackPosition);
     }
 
     public void attackBotBoard(string attackPosition)
     {
       this.BotBoard.attack(attackPosition);
-    }
-
-    public void printBotBoard()
-    {
-      this.BotBoard.hideShips();
-
-      this.telegramPrinter.printBoard(this.BotBoard, this.Player.ID);
     }
 
     public void AddShipToBoard(List<string> positions)
@@ -53,16 +74,16 @@ namespace ChatBotProject
           new List<string>() {"B3", "B4", "B5", "B6", "B7"}
         },
         new List<List<string>>() {
-          new List<string>() {"J1", "J2"},
-          new List<string>() {"H1", "H2", "H3"},
-          new List<string>() {"H10", "G10", "I10", "J10"},
-          new List<string>() {"B3", "B4", "B5", "B6", "B7"}
+          new List<string>() {"I1", "I2"},
+          new List<string>() {"C4", "C5", "C6"},
+          new List<string>() {"D10", "D9", "D8", "D7"},
+          new List<string>() {"J2", "I2", "H2", "G2", "F2"}
         },
         new List<List<string>>() {
-          new List<string>() {"J1", "J2"},
-          new List<string>() {"H1", "H2", "H3"},
-          new List<string>() {"H10", "G10", "I10", "J10"},
-          new List<string>() {"B3", "B4", "B5", "B6", "B7"}
+          new List<string>() {"F5", "F6"},
+          new List<string>() {"A1", "A2", "A3"},
+          new List<string>() {"A10", "B10", "C10", "D10"},
+          new List<string>() {"J8", "I8", "H8", "G8", "F8"}
         }
       };
 
@@ -76,12 +97,12 @@ namespace ChatBotProject
 
     public bool playerBoardHasShips()
     {
-      return this.PlayerBoard.getShips().Count != 0;
+      return this.PlayerBoard.getShips().Count > 0;
     }
 
     public bool botBoardHasShips()
     {
-      return this.BotBoard.getShips().Count != 0;
+      return this.BotBoard.getShips().Count > 0;
     }
 
     public void StartGame()
