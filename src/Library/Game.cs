ffcf3a5bf42fcf3b418ba7c7a3caf635db1ebcf2
RodrigoCD16 <53954034+RodrigoCD16@users.noreply.github.com>
2022-07-05 08:47:25 -0300

    Update

--- src/Library/Game.cs
+++ src/Library/Game.cs
@@ -8,6 +8,7 @@ namespace ChatBotProject
     private bool InProgress;
     private User Winner;
     private Dictionary<User, Board> InMatchUsersBoard;
+    private TelegramPrinter telegramPrinter;
 
     public Game(List<User> InMatchUsers, int totalMins, int totalSecs, int minsPerRound, int secsPerRound)
     {
@@ -18,7 +19,7 @@ namespace ChatBotProject
         this.InMatchUsersBoard = new Dictionary<User, Board>();
         this.Time = new Timer(totalMins, totalSecs);
         this.TimePerRound = new Timer(minsPerRound, secsPerRound);
-
+        this.telegramPrinter = new TelegramPrinter();
         // Llenar el diccionario con clave User y Valor Board
         foreach(User user in this.InMatchUsers)
         {
@@ -153,6 +154,73 @@ namespace ChatBotProject
 
       return allShipsPositions;
     }
+
+
+    public void PrintPlayerBoardToSelf()
+    {
+      Board firstUserBoard = getUserBoard(this.InMatchUsers[0]);
+      firstUserBoard.showShips();
+      this.telegramPrinter.printBoard(firstUserBoard, this.InMatchUsers[0].ID);
+    }
+
+    public void PrintPlayerBoardToEnemy()
+    {
+      Board firstUserBoard = getUserBoard(this.InMatchUsers[0]);
+      firstUserBoard.hideShips();
+      this.telegramPrinter.printBoard(firstUserBoard, this.InMatchUsers[1].ID);
+    }
+
+    public void PlayerAddShipToBoard(List<string> positions)
+    {
+      Board firstUserBoard = getUserBoard(this.InMatchUsers[0]);
+      firstUserBoard.addShip(positions);
+    }
+
+    public bool PlayerBoardHasShips()
+    {
+      Board firstUserBoard = getUserBoard(this.InMatchUsers[0]);
+      return firstUserBoard.getShips().Count != 0;
+    }
+
+    public void AttackRivalPlayerBoard(string attackPosition)
+    {
+      Board secondUserBoard = getUserBoard(this.InMatchUsers[1]);
+      secondUserBoard.attack(attackPosition);
+    }
+
+    public void PrintRivalPlayerBoardToSelf()
+    {
+      Board secondUserBoard = getUserBoard(this.InMatchUsers[1]);
+      secondUserBoard.showShips();
+      this.telegramPrinter.printBoard(secondUserBoard, this.InMatchUsers[1].ID);
+    }
+
+    public void PrintRivalPlayerBoardToEnemy()
+    {
+      Board secondUserBoard = getUserBoard(this.InMatchUsers[1]);
+      secondUserBoard.hideShips();
+      this.telegramPrinter.printBoard(secondUserBoard, this.InMatchUsers[0].ID);
+    }
+
+    public void RivalPlayerAddShipToBoard(List<string> positions)
+    {
+      Board secondUserBoard = getUserBoard(this.InMatchUsers[1]);
+      secondUserBoard.addShip(positions);
+    }
+
+    public bool RivalPlayerBoardHasShips()
+    {
+      Board secondUserBoard = getUserBoard(this.InMatchUsers[1]);
+      return secondUserBoard.getShips().Count != 0;
+    }
+
+    public void AttackPlayerBoard(string attackPosition)
+    {
+      Board firstUserBoard = getUserBoard(this.InMatchUsers[0]);
+      firstUserBoard.attack(attackPosition);
+    }
+
+    
 
     // Winner Getters & Setters
     public User getWinner()
